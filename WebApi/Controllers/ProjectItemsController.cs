3c6ddd4ee6c799331b1df9154abb881a4f4f1957
Mykhailo Kravchenko <mykhailo.kravchenko@oriflame.com>
2020-05-27 18:54:10 +0300

    Scafold CRUD opoerations

---
+++ WebApi/Controllers/ProjectItemsController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApi.Data;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectItemsController : ControllerBase
+    {
+        private readonly ProjectsDbContext _context;
+
+        public ProjectItemsController(ProjectsDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ProjectItems
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProjectItem>>> GetProjectItems()
+        {
+            return await _context.ProjectItems.ToListAsync();
+        }
+
+        // GET: api/ProjectItems/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProjectItem>> GetProjectItem(int id)
+        {
+            var projectItem = await _context.ProjectItems.FindAsync(id);
+
+            if (projectItem == null)
+            {
+                return NotFound();
+            }
+
+            return projectItem;
+        }
+
+        // PUT: api/ProjectItems/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProjectItem(int id, ProjectItem projectItem)
+        {
+            if (id != projectItem.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(projectItem).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProjectItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ProjectItems
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<ProjectItem>> PostProjectItem(ProjectItem projectItem)
+        {
+            _context.ProjectItems.Add(projectItem);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProjectItem", new { id = projectItem.Id }, projectItem);
+        }
+
+        // DELETE: api/ProjectItems/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<ProjectItem>> DeleteProjectItem(int id)
+        {
+            var projectItem = await _context.ProjectItems.FindAsync(id);
+            if (projectItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.ProjectItems.Remove(projectItem);
+            await _context.SaveChangesAsync();
+
+            return projectItem;
+        }
+
+        private bool ProjectItemExists(int id)
+        {
+            return _context.ProjectItems.Any(e => e.Id == id);
+        }
+    }
+}
